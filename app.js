f51fc8ff298beb2266f41f95af71bc6a3921dced
Deeban23 <deebanchakravarthi23@gmail.com>
2023-07-21 18:20:34 +0530

    Back

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Parse incoming requests with JSON payloads
 app.use(bodyParser.json());
